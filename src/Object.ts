87f486e5e80446639d33fdadb99f282ad01f53a1
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-05-29 16:46:06 +0800

    feat: Update Content

---
+++ src/Object.ts
@@ -0,0 +1,11 @@
+export const MyObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+}
+
+const objectKeys = <Obj>(obj: Obj): (keyof Obj)[] => Object.keys(obj) as (keyof Obj)[]
+
+objectKeys(MyObject).forEach((key) => {
+  console.log(MyObject[key])
+})
